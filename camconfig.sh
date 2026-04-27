43cb8ca502781069b1b5795c551077d6c3731f60
Asal Mirzaieva <4975074+Asalle@users.noreply.github.com>
2026-04-27 14:01:53 +0200

    Export csi2 and ip variables in camconfig.sh
    Add exports for csi2 and ip variables from video4linux.

--- camconfig.sh
+++ camconfig.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+export csi2=$(cat /sys/class/video4linux/v4l-subdev*/name | grep "csi2" | head -n 1)
+export ip=$(cat /sys/class/video4linux/v4l-subdev*/name | grep "cru-ip" | head -n 1)
 export RESOLUTION="2592x1944"
+
 /usr/bin/media-ctl -d /dev/media0 -r
 /usr/bin/media-ctl -d /dev/media0 -l "'${csi2}':1 -> '${ip}':0 [1]"
 /usr/bin/media-ctl -d /dev/media0 -V "'${csi2}':1 [fmt:UYVY8_2X8/${RESOLUTION} field:none]"
